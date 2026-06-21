d4efe3d7a0e14feec8ded24b350e3739e1926274
gitc-azz <graphical.it.consulting@proton.me>
2026-06-21 09:31:29 +0200

    feat: maxed out at 60 fps

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0.0 # in second delta time
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,6 +16,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
