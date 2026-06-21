0825fdbdc7ebac9cdf8cfa38ebf8cf80ca6416be
gitc-azz <graphical.it.consulting@proton.me>
2026-06-21 11:07:48 +0200

    feat: centered player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
     
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     clock = pygame.time.Clock()
     dt = 0.0 # in second delta time
@@ -14,7 +16,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
